5d39c2a03725647bcb6ff5cb7a17096266a638a5
nkr-jgr <nkr.jgr@gmail.com>
2023-10-21 21:24:32 +0530

    added name

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Naresh Pahariya</p>
         <p>
           (This is a sample website - you'll be building a site like this on {' '}
           <a href="href://nextjs.org/learn">our Next.js tutorial</a>.)
