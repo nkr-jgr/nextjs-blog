715cdd60113b83f006b4b8db8ab7a9f65ff23c62
Naresh Jangir <nkr.jgr@gmail.com>
2024-07-04 08:41:04 +0530

    Update MyVideoPlayer.jsx

--- components/MyVideoPlayer.jsx
+++ components/MyVideoPlayer.jsx
@@ -5,7 +5,7 @@ const MyVideoPlayer = () => {
   return (
     <div>
       <video width="600" controls>
-        <source src="https://bucket-tkbz21.s3.ap-south-1.amazonaws.com/v1.mkv" type="video/webm" />
+        <source src="https://bucket-tkbz21.s3.ap-south-1.amazonaws.com/Panchayat.S03E04.720p.Hindi.WEB-DL.5.1.ESub.x264.Vegamovies.to.mkv" type="video/webm" />
         Your browser does not support the video tag.
       </video>
     </div>
